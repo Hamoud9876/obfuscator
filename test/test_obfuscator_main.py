bd8335358924f704c2e8d36c4008fbf343169581
Hamoud9876 <h.ghaleb95@gmail.com>
2025-09-22 22:03:21 +0100

    added a new test to search not finding anything

--- test/test_obfuscator_main.py
+++ test/test_obfuscator_main.py
@@ -1,5 +1,7 @@
 from src.obfuscator_main import obfuscator_main
 import pytest
+from unittest.mock import MagicMock, patch
+import logging
 
 
 @pytest.fixture(scope="function")
@@ -37,7 +39,29 @@ class TestObfuscator():
 
     
     
-    def test_return_byte_stream(self,create_data):
+    # def test_return_byte_stream(self,create_data):
+    #     j_string = create_data
+    #     obfuscator_main(j_string)
+
+
+    @patch("src.obfuscator_main.re.compile")
+    def test_format_not_found(self,pattern_mock,caplog, create_data):
         j_string = create_data
-        obfuscator_main(j_string)
+
+        caplog.set_level(logging.INFO)
+
+        my_mock = MagicMock()
+        my_mock.search.return_value = None
+        pattern_mock.return_value = my_mock
+
+
+        with pytest.raises(Exception) as e:
+            obfuscator_main(j_string)
+
+
+        assert type(e.value) == AttributeError
+        assert "no file format was found" in caplog.text
+
+
+
         
