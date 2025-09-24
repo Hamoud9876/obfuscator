4fb9c5715ebafa2ed2ddb309d11e6917ad97182a
Hamoud9876 <h.ghaleb95@gmail.com>
2025-09-25 00:40:16 +0100

    the test for ectract file format

---
+++ test/test_extract_file_format.py
@@ -0,0 +1,53 @@
+import logging
+from unittest.mock import MagicMock, patch
+from utils.extract_file_format import extract_file_format
+import pytest
+import json
+
+
+
+@pytest.fixture(scope="function")
+def create_data():
+    json_string = '''{
+        "file_to_obfuscator": "s3://test_bucket/my_file.csv",
+        "pii_fields": ["name", "id","email"]
+    }'''
+
+    yield json_string
+
+
+@patch("utils.extract_file_format.re.compile")
+def test_format_not_found(pattern_mock,caplog, create_data):
+    j_string = create_data
+    jstring = json.loads(j_string)
+
+    caplog.set_level(logging.INFO)
+
+    my_mock = MagicMock()
+    my_mock.search.return_value = None
+    pattern_mock.return_value = my_mock
+
+
+    response = extract_file_format(jstring["file_to_obfuscator"])
+
+
+    assert response == None
+    assert "no file format was found" in caplog.text
+
+def test_return_correct_format(create_data):
+    #csv
+    j_string = create_data
+    jstring = json.loads(j_string)
+
+    response = extract_file_format(jstring["file_to_obfuscator"])
+
+
+    assert response == "csv"
+
+    #json
+    txt = '{"file_to_obfuscator": "s3://test_bucket/my_file.json"}'
+    jstring = json.loads(txt)
+
+    response = extract_file_format(jstring["file_to_obfuscator"])
+
+    assert response == "json"
