2db9eb06b516b21ec8b7771fdbf17d6bb624e051
Hamoud9876 <h.ghaleb95@gmail.com>
2025-09-25 00:39:29 +0100

    split the logic that extracts the file format from the s3 url

---
+++ utils/extract_file_format.py
@@ -0,0 +1,37 @@
+import re
+import logging
+
+logging.basicConfig(
+    filename="app.log",
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    force=True
+)
+
+logger = logging.getLogger(__name__)
+
+def extract_file_format(file_string):
+    """
+    Searchs for file format in json key 'file_to_obfuscator'
+
+    Args:
+        file_string: full s3 url location for the file
+        that ends with the format
+
+    Return:
+        String representing the file format (e.g. csv, json, parquet)
+    """
+
+    #RegEx pattern to find any file format
+    pattern = re.compile(r"\.([a-zA-Z0-9]+)$")
+    match_ = pattern.search(file_string)
+
+    #ruling out AttributeError if no match found
+    if not match_:
+        logger.error(f"no file format was found")
+        return None
+
+    #fetching the file format from capture group
+    file_format = match_.group(1)
+
+    return file_format
