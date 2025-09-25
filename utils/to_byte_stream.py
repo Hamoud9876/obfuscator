59567818267c2836bf889be04afeb6f0df7914b4
Hamoud9876 <h.ghaleb95@gmail.com>
2025-09-26 00:43:39 +0100

    ran black and flake8

--- utils/to_byte_stream.py
+++ utils/to_byte_stream.py
@@ -6,13 +6,13 @@ logging.basicConfig(
     filename="app.log",
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
-    force=True
+    force=True,
 )
 
 logger = logging.getLogger(__name__)
 
 
-def to_byte_stream(df: pd.DataFrame, format:str)-> io.BytesIO:
+def to_byte_stream(df: pd.DataFrame, format: str) -> io.BytesIO:
     """
     convert df into byte stream of the provided format
 
@@ -20,33 +20,32 @@ def to_byte_stream(df: pd.DataFrame, format:str)-> io.BytesIO:
         df: dataframe contaning file content
         format: the format the byte esteam
         should be in
-    
+
     Return:
-        REady to read byte stream of the same
+        Ready to read byte stream of the same
         content as the df
     """
-    #taking a deep copy not not mutate the original
+    # taking a deep copy not not mutate the original
     df_copy = df.copy(deep=True)
 
-    #creating a buffer to store df content on
+    # creating a buffer to store df content on
     buffer = io.BytesIO()
     try:
         match format:
             case "csv":
-                df_copy.to_csv(buffer,index=False)
+                df_copy.to_csv(buffer, index=False)
             case "json":
-                df_copy.to_json(buffer, orient="records", lines=True)
+                df_copy.to_json(buffer, orient="records")
             case "parquet":
-                df.to_parquet(buffer, index=False, engine='pyarrow')
+                df.to_parquet(buffer, index=False, engine="pyarrow")
             case _:
                 logger.error(f"No Byte stream format matches {format}")
                 return None
-            
+
     except Exception:
-        logger.error('Something went wrong ,', exc_info=True)
+        logger.error("Something went wrong ,", exc_info=True)
         return None
-    
+
     buffer.seek(0)
 
     return buffer
-
