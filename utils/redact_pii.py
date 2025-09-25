d62d34c0c31581282c269ff312f3d8a4aa3a87f1
Hamoud9876 <h.ghaleb95@gmail.com>
2025-09-25 02:46:12 +0100

    created the logic to redact pii fields dynamically

---
+++ utils/redact_pii.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import logging
+
+logging.basicConfig(
+    filename="app.log",
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    force=True
+)
+
+logger = logging.getLogger(__name__)
+
+
+
+def redact_pii(df_file: pd.DataFrame, pii_fields: list) -> pd.DataFrame:
+    """
+    redact the pii field in the df based on the
+    pii_field input
+
+    Args:
+        df_file: DataFrame contaning file content
+        pii_fields: list contaning fields to be redaced
+
+    Return:
+        Dataframe with pii fields being redacted
+    """
+
+    #taking a deep copy to avoid mutating original df
+    copy_df = df_file.copy(deep=True)
+
+
+    #looping though pii list and redacing each column
+    for field in  pii_fields:
+        if field in copy_df.columns:
+            copy_df[field] = '***'
+        else:
+            logger.warning(f"pii field: {field} was not found")
+
+
+    return copy_df
