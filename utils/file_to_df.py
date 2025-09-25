ce3d2b1576709f760c5f3bc8be76311ab6747815
Hamoud9876 <h.ghaleb95@gmail.com>
2025-09-26 00:42:57 +0100

    ran black and flake8

--- utils/file_to_df.py
+++ utils/file_to_df.py
@@ -7,11 +7,10 @@ logging.basicConfig(
     filename="app.log",
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
-    force=True
+    force=True,
 )
 
 logger = logging.getLogger(__name__)
-
 
 
 def file_to_df(file_body, format: str) -> pd.DataFrame:
@@ -35,14 +34,14 @@ def file_to_df(file_body, format: str) -> pd.DataFrame:
             case "csv":
                 df = pd.read_csv(file_body)
             case "json":
-                df = pd.read_json(file_body,orient="records", lines=True)
+                df = pd.read_json(file_body, orient="records")
             case "parquet":
                 buffer = io.BytesIO(file_body.read())
                 df = pd.read_parquet(buffer, engine="pyarrow")
             case _:
                 logger.warning("The provided format doesn't match")
                 return None
-    except Exception as e:
+    except Exception:
         logger.error("Something went wrong ", exc_info=True)
         return None
 
