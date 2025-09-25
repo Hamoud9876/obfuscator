34d3f9d2b4273974cb6929d2a27419acdd4f490b
Hamoud9876 <h.ghaleb95@gmail.com>
2025-09-26 00:43:15 +0100

    ran black and flake8

--- utils/get_file_content.py
+++ utils/get_file_content.py
@@ -9,10 +9,11 @@ logging.basicConfig(
     filename="app.log",
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
-    force=True
+    force=True,
 )
 
 logger = logging.getLogger(__name__)
+
 
 def get_file_content(flocation: str) -> Optional[StreamingBody]:
     """
@@ -26,45 +27,36 @@ def get_file_content(flocation: str) -> Optional[StreamingBody]:
         returns None if failed at any point
     """
 
-    #checking if the input is of type string
-    if not isinstance(flocation,str):
+    # checking if the input is of type string
+    if not isinstance(flocation, str):
         logger.error("wrong input type for 'flocation'")
         return None
 
-    
-    #regex pattern, returns bucket name in group 1
-    #and key in group 2
+    # regex pattern, returns bucket name in group 1
+    # and key in group 2
     pattern = re.compile(r"^s3://([^/]+)/(.+)$")
     match_ = pattern.match(flocation)
 
-    
-    #ruling out AttributeError if no match found
+    # ruling out AttributeError if no match found
     if not match_:
         logger.error(f"Invalid S3 URL: {flocation}")
         return None
 
-
-    #getting bucket name and key from the capture groups
+    # getting bucket name and key from the capture groups
     bucket = match_.group(1)
     key = match_.group(2)
 
-
     try:
-        #creating boto3 client
+        # creating boto3 client
         s3_client = boto3.client("s3")
 
+        # retrieving the file content from the bucket
+        s3_response = s3_client.get_object(Bucket=bucket, Key=key)
 
-        #retrieving the file content from the bucket
-        s3_response = s3_client.get_object(
-            Bucket= bucket,
-            Key=key
+    except Exception:
+        logger.error(
+            f"Failed to retrieve S3 object {bucket}/{key}", exc_info=True
         )
-
-
-    except Exception as e:
-        logger.error(f"Failed to retrieve S3 object {bucket}/{key}", 
-                     exc_info=True)
         return None
-
 
     return s3_response["Body"]
