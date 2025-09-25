c58c65ce0b780025cb52f6a7ef2501f92bb2e9ec
Hamoud9876 <h.ghaleb95@gmail.com>
2025-09-26 00:43:24 +0100

    ran black and flake8

--- utils/redact_pii.py
+++ utils/redact_pii.py
@@ -5,11 +5,10 @@ logging.basicConfig(
     filename="app.log",
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
-    force=True
+    force=True,
 )
 
 logger = logging.getLogger(__name__)
-
 
 
 def redact_pii(df_file: pd.DataFrame, pii_fields: list) -> pd.DataFrame:
@@ -25,16 +24,14 @@ def redact_pii(df_file: pd.DataFrame, pii_fields: list) -> pd.DataFrame:
         Dataframe with pii fields being redacted
     """
 
-    #taking a deep copy to avoid mutating original df
+    # taking a deep copy to avoid mutating original df
     copy_df = df_file.copy(deep=True)
 
-
-    #looping though pii list and redacing each column
-    for field in  pii_fields:
+    # looping though pii list and redacing each column
+    for field in pii_fields:
         if field in copy_df.columns:
-            copy_df[field] = '***'
+            copy_df[field] = "***"
         else:
             logger.warning(f"pii field: {field} was not found")
-
 
     return copy_df
