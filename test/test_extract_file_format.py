dbef33ee2b54777f9dbdbb9a8b1af7e309edfc72
Hamoud9876 <h.ghaleb95@gmail.com>
2025-09-26 00:38:12 +0100

    ran black and flake8

--- test/test_extract_file_format.py
+++ test/test_extract_file_format.py
@@ -5,19 +5,18 @@ import pytest
 import json
 
 
-
 @pytest.fixture(scope="function")
 def create_data():
-    json_string = '''{
+    json_string = """{
         "file_to_obfuscator": "s3://test_bucket/my_file.csv",
         "pii_fields": ["name", "id","email"]
-    }'''
+    }"""
 
     yield json_string
 
 
 @patch("utils.extract_file_format.re.compile")
-def test_format_not_found(pattern_mock,caplog, create_data):
+def test_format_not_found(pattern_mock, caplog, create_data):
     j_string = create_data
     jstring = json.loads(j_string)
 
@@ -27,24 +26,22 @@ def test_format_not_found(pattern_mock,caplog, create_data):
     my_mock.search.return_value = None
     pattern_mock.return_value = my_mock
 
-
     response = extract_file_format(jstring["file_to_obfuscator"])
-
 
     assert response == None
     assert "no file format was found" in caplog.text
 
+
 def test_return_correct_format(create_data):
-    #csv
+    # csv
     j_string = create_data
     jstring = json.loads(j_string)
 
     response = extract_file_format(jstring["file_to_obfuscator"])
 
-
     assert response == "csv"
 
-    #json
+    # json
     txt = '{"file_to_obfuscator": "s3://test_bucket/my_file.json"}'
     jstring = json.loads(txt)
 
